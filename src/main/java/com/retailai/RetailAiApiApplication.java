54a718151a5daf7d8c0f900663220e345c51883f
Mcspencer Nyaoso <mcspencernyaoso@Mcspencers-MacBook-Air.local>
2026-05-17 13:02:24 -0500

    Disable default generated security user

--- src/main/java/com/retailai/RetailAiApiApplication.java
+++ src/main/java/com/retailai/RetailAiApiApplication.java
@@ -2,9 +2,11 @@ package com.retailai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class RetailAiApiApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(RetailAiApiApplication.class, args);
     }
